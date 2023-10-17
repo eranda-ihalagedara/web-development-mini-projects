8e696271866b0564f94ff4873b60db021b01a580
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-10-18 10:43:19 +1100

    Add perspective change to wider viewpoint

--- CSS 3D/scripts.js
+++ CSS 3D/scripts.js
@@ -1,7 +1,7 @@
 // Get the element with id "output"
 let viewpoint = document.getElementById("viewpoint");
 let panel = document.getElementById("panel");
-let txt = document.getElementById("mouse_loc");
+// let txt = document.getElementById("mouse_loc");
 
 // Add a "mousemove" event listener
 
@@ -14,17 +14,9 @@ viewpoint.onmousemove = function(event) {
   const yPos = event.clientY - (rect.top + viewpoint.offsetHeight/2);
 
   // panel.style.transform = `rotateX(${yPos/5}deg) rotateY(${xPos/10}deg)`;
-  viewpoint.style.perspectiveOrigin = `${xPos/2}% ${yPos/2}%`;
+  viewpoint.style.perspectiveOrigin = `${50 + xPos/10}% ${50 + yPos/5}%`;
   // panel.style.perspectiveOrigin = `50% 50%`;
 
   // txt.innerHTML = xPos + "," + yPos + " | " + event.clientX + ","+event.clientY + " | " + rect.top + "," + rect.left;
     
 };
-
-
-//   viewpoint.onmouseleave = function(event) {
-//     // Get the X and Y coordinates of the mouse
-    
-//     panel.style.transform = `rotateX(${yPos/}deg) rotateY(${xPos/2}deg)`;
-
-//   };
