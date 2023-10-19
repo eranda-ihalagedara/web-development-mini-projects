c0899d77b2700ffd4e23ea7a55e7abea1ba12857
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-10-19 23:32:53 +1100

    Clean js

--- CSS 3D/scripts.js
+++ CSS 3D/scripts.js
@@ -13,10 +13,8 @@ viewpoint.onmousemove = function(event) {
   const xPos = event.clientX - (rect.left + viewpoint.offsetWidth/2);
   const yPos = event.clientY - (rect.top + viewpoint.offsetHeight/2);
 
-  // panel.style.transform = `rotateX(${yPos/5}deg) rotateY(${xPos/10}deg)`;
   viewpoint.style.perspectiveOrigin = `${50 + xPos/10}% ${50 + yPos/5}%`;
-  // panel.style.perspectiveOrigin = `50% 50%`;
-
+ 
   // txt.innerHTML = xPos + "," + yPos + " | " + event.clientX + ","+event.clientY + " | " + rect.top + "," + rect.left;
     
 };
