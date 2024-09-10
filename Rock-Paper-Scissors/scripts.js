dfd27c941bd9f3f6633dc391b14810368f1fcdcb
Eranda Ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2024-09-10 12:07:05 +1000

    Add basic game play

--- Rock-Paper-Scissors/scripts.js
+++ Rock-Paper-Scissors/scripts.js
@@ -1,1 +1,45 @@
 console.log("Hello, World!");
+const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper, or scissors");
+
+    return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+
+        humanScore++;
+        return "You win! " + humanChoice + " beats " + computerChoice;
+    } else {
+        computerScore++;
+        return "You lose! " + computerChoice + " beats " + humanChoice;
+    }
+
+}
+
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        const result = playRound(humanChoice, computerChoice);
+        alert(result);
+    }
+
+    alert("Your score: " + humanScore + " Computer score: " + computerScore);
+}
+
+playGame();
