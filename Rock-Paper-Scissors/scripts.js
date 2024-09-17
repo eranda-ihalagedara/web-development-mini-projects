e0e24f12c50d37fbaa17eac2ceba5fa5f1fc75b0
Eranda Ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2024-09-17 17:55:32 +1000

    Add basic UI

--- Rock-Paper-Scissors/scripts.js
+++ Rock-Paper-Scissors/scripts.js
@@ -30,16 +30,45 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
-function playGame() {
+const rockButton = document.querySelector("#btn-rock");
+const paperButton = document.querySelector("#btn-paper");
+const scissorsButton = document.querySelector("#btn-scissors");
+const resetButton = document.querySelector("#btn-reset");
+const resultPara = document.querySelector("#results");
+const scoreCard = document.querySelector("#score");
 
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        const result = playRound(humanChoice, computerChoice);
-        alert(result);
-    }
+rockButton.addEventListener("click",(e)=>{
+    const result = playRound("rock", getComputerChoice());
+    resultPara.innerHTML = result+"<br>" + resultPara.innerHTML;
+    scoreCard.textContent = `Your score: ${humanScore} Computer score: ${computerScore}`;
+});
+paperButton.addEventListener("click",(e)=>{
+    const result = playRound("paper", getComputerChoice());
+    resultPara.innerHTML = result+"<br>" + resultPara.innerHTML;
+    scoreCard.textContent = `Your score: ${humanScore} Computer score: ${computerScore}`;
+});
+scissorsButton.addEventListener("click",(e)=>{
+    const result = playRound("scissors", getComputerChoice());
+    resultPara.innerHTML = result+"<br>" + resultPara.innerHTML;
+    scoreCard.textContent = `Your score: ${humanScore} Computer score: ${computerScore}`;
+});
 
-    alert("Your score: " + humanScore + " Computer score: " + computerScore);
-}
+resetButton.addEventListener("click", (e)=>{
+    humanScore = 0;
+    computerScore = 0;
+    scoreCard.textContent = "";
+    resultPara.textContent = "";
+});
+// function playGame() {
 
-playGame();
+//     for (let i = 0; i < 5; i++) {
+//         const humanChoice = getHumanChoice();
+//         const computerChoice = getComputerChoice();
+//         const result = playRound(humanChoice, computerChoice);
+//         alert(result);
+//     }
+
+//     alert("Your score: " + humanScore + " Computer score: " + computerScore);
+// }
+
+//playGame();
