718f6efb6bcd2ea2d054714787ea2a218e51641b
Eranda Ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2024-09-24 11:07:18 +1000

    Etch-a-Sketch Add color picker

--- Etch-a-Sketch/scripts.js
+++ Etch-a-Sketch/scripts.js
@@ -1,34 +1,32 @@
 let gridSize = 16;
-let penColor = 'black';
+let penColor = '#000000';
+let curColor = '#000000';
+let isErasing = false;
 let opacity = 100;
-let opacitySign = 1;
 let isOverPad = false;
 let isMouseDown = false;
 
-const containerDiv = document.querySelector('#container');
+const containerDiv = document.querySelector('#grid');
 containerDiv.classList.add('pen-enabled');
 
-// Stop drawing when mouse leaves the pad
-containerDiv.addEventListener('mouseleave', (e) => {
+containerDiv.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
+});
+containerDiv.addEventListener('mouseup', (e) => {
     isMouseDown = false;
-})
+});
+
 
 function createGrid(size) {
     for (let i = 0; i < size ; i++) {
         const row = document.createElement('div');
-        row.setAttribute('class', 'flex-row');
+        row.setAttribute('class', 'grid-row');
         for(let j = 0; j < size ; j++) {
             const div = document.createElement('div');
-            div.setAttribute('class', 'flex-item');
+            div.setAttribute('class', 'grid-tile');
             div.style.opacity = 0;
             
-            div.addEventListener('mousedown', (e) => {
-                isMouseDown = true;
-                drawOnTile(e, penColor);
-            });
-            div.addEventListener('mouseup', () => {
-                isMouseDown = false;
-            });
+            div.addEventListener('mousedown', (e) => {drawOnTile(e, penColor)});
             div.addEventListener('mouseover', (e) => {
                 if(!isMouseDown) return;
                 drawOnTile(e, penColor);
@@ -41,8 +39,13 @@ function createGrid(size) {
 }
 
 function drawOnTile(e, color) {
-    e.target.style.backgroundColor = color;
-    e.target.style.opacity = Math.max(Math.min(Number.parseFloat(e.target.style.opacity) + opacitySign*opacity / 100, 1), 0);
+    if(!isErasing) {
+        e.target.style.backgroundColor = color;
+        e.target.style.opacity = Math.max(Math.min(Number.parseFloat(e.target.style.opacity) + opacity / 100, 1), 0);
+    } else{
+        e.target.style.backgroundColor = 'white';
+        e.target.style.opacity = '0';
+    }
 }
 
 createGrid(gridSize);
@@ -53,24 +56,20 @@ eraseButton.addEventListener('click', (e) => {
     
     containerDiv.classList.toggle('pen-enabled');
     containerDiv.classList.toggle('eraser-enabled');
-    opacitySign *= -1;
 
-    penColor = ((curColor) => {
-        if (curColor == 'black') return 'white';
-        return 'black';
-    })(penColor);
+    isErasing = !isErasing;
 
-    if(eraseButton.textContent == "Erase") {
-        eraseButton.textContent = "Draw";
+    if(isErasing) {
+        eraseButton.textContent = "Pen";
     } else {
-        eraseButton.textContent = "Erase";
+        eraseButton.textContent = "Eraser";
     }
 });
 
 // Clear pad
 const clearButton = document.querySelector('#btn-clear');
 clearButton.addEventListener('click', (e) => {
-    const tiles = document.querySelectorAll('.flex-item');
+    const tiles = document.querySelectorAll('.grid-tile');
     tiles.forEach((tile) => {
         tile.style.backgroundColor = 'white';
         tile.style.opacity = '0';
@@ -78,35 +77,17 @@ clearButton.addEventListener('click', (e) => {
     containerDiv.classList.add('pen-enabled');
     containerDiv.classList.remove('eraser-enabled');
 
-    penColor = 'black';
+    penColor = '#000000';
     opacitySign = 1;
-    eraseButton.textContent = "Erase";
+    isErasing = false;
+    eraseButton.textContent = "Eraser";
 });
 
-// Change grid size
-const resolutionButton = document.querySelector('#btn-resolution');
-resolutionButton.addEventListener('click', (e) => {
-    let newGridSize = prompt("Enter new grid size(4 - 64):");
-    newGridSize = parseInt(newGridSize, 10);
-
-    // Validate if input is a number and an integer
-    if (isNaN(newGridSize) || !Number.isInteger(newGridSize)) {
-        alert("Invalid input! Please enter a valid integer.");
-        return;
-    }
-
-    if(newGridSize < 4 || newGridSize > 64) {
-        alert("Grid size must be between 4 and 64");
-        return;
-    }
-
-    while (containerDiv.firstChild) {
-        containerDiv.removeChild(containerDiv.firstChild);
-    }
-
-    gridSize = newGridSize;
-    createGrid(gridSize);
-
+// Change color
+const colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener('input', (e) => {
+    curColor = e.target.value;
+    penColor = curColor;
 });
 
 // Change opacity
@@ -114,5 +95,21 @@ const opacityRange = document.querySelector('#opacity-range');
 const opacityValue = document.querySelector('#opacity-value');
 opacityRange.addEventListener('input', (e) => {
     opacity = e.target.value;
-    opacityValue.textContent = opacity;
+    opacityValue.textContent = `Opacity: ${opacity}`;
+});
+
+// Change grid size
+const gridSizeRange = document.querySelector('#grid-size-range');
+const gridSizeLabel = document.querySelector('#grid-size-label');
+gridSizeRange.addEventListener('input', (e) => {
+    gridSize = parseInt(e.target.value, 10);
+    gridSizeLabel.textContent = `${gridSize} x ${gridSize}`;
+});
+
+gridSizeRange.addEventListener('change', (e) => {
+    
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+    createGrid(gridSize);
 });
